ec82e0e1b3b3efcae8a727987fba71e93330db2a
1026minjae <minjae2k1@gmail.com>
2020-09-05 15:30:25 +0900

    DB 연동 제외 다 만듬. 색깔 좀 더 연하게 수정.

--- ScheduleBell/MainWindow.xaml.cs
+++ ScheduleBell/MainWindow.xaml.cs
@@ -20,11 +20,94 @@ namespace ScheduleBell
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    
+    public class Schedule
+    {
+        public bool isDate = false; //false: 요일 지정. true: 날짜 지정. 날짜 지정이 높은 우선순위.
+        private int starthour = 0; //value: 0~23
+        private int startmin = 0; //value: 0-59
+        private int endhour = 0; //value:0~23
+        private int endmin = 0; //value: 0-59
+        public string sche;
+
+        public Schedule(bool id, int sh, int sm, int eh, int em, string sch)
+        {
+            isDate = id;
+            starthour = sh;
+            startmin = sm;
+            endhour = eh;
+            endmin = em;
+            sche = sch;
+        }
+
+        public bool IsTime(int currhour, int currmin)
+        {
+            if(starthour > currhour || currhour > endhour)
+            {
+                return false;
+            }
+            else if (starthour < currhour && currhour < endhour)
+            {
+                return false;
+            }
+            else if(starthour == currhour && startmin <= currmin)
+            {
+                return true;
+            }
+            else if(currhour == endhour && currmin <= endmin)
+            {
+                return true;
+            }
+            return true;
+        }
+    }
+
     public partial class MainWindow : Window
     {
+        private List<Schedule> schedules = new List<Schedule>();
+
+        private void ReadScheDB()
+        {
+            //현재 해당되는 일정을 DB에서 가져온다.
+            //string query = "SELECT * FROM TABLE WHERE year=yyyy and month=mm and day=dd or dow=d;";
+            //텍스트, 시작시점, 종료시점, 타입을 가져온다.
+            //schedules 할당. 담는다.
+            //하루마다 갱신한다.
+
+            //sample
+            schedules.Add(new Schedule(false, 15, 0, 16, 0, "일정 테스트 용도입니다."));
+            schedules.Add(new Schedule(true, 14, 30, 15, 30, "날짜 지정 테스트 용도입니다."));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            ReadScheDB();
+        }
+
+        private void SetClock(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate {
+                cloc.Text = DateTime.Now.ToString();
+            }));
+        }
+
+        private void SetSche(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate {
+                bool isDate = false;
+                foreach(Schedule sc in schedules)
+                {
+                    if(sc.IsTime(DateTime.Now.Hour, DateTime.Now.Minute))
+                    {
+                        if(!isDate)
+                        {
+                            sche.Text = sc.sche;
+                            isDate = sc.isDate;
+                        }
+                    }
+                }
+            }));
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -33,24 +116,22 @@ namespace ScheduleBell
             this.Left = screen.Right - this.Width;
             this.Top = 0;
 
-            DateTime time = DateTime.Now;
-            cloc.Text = time.ToString();
-
             System.Timers.Timer timer = new Timer(TimeSpan.FromSeconds(1).TotalMilliseconds);
             timer.AutoReset = true;
             timer.Elapsed += new System.Timers.ElapsedEventHandler(SetClock);
             timer.Start();
-        }
+            SetClock(sender, new EventArgs() as ElapsedEventArgs);
 
-        private void SetClock(object sender, ElapsedEventArgs e)
-        {
-            Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate { cloc.Text = DateTime.Now.ToString(); }));
+            System.Timers.Timer timer2 = new Timer(TimeSpan.FromMinutes(1).TotalMilliseconds);
+            timer2.AutoReset = true;
+            timer2.Elapsed += new System.Timers.ElapsedEventHandler(SetSche);
+            timer2.Start();
+            SetSche(sender, new EventArgs() as ElapsedEventArgs);
         }
 
         private void Window_Drag(object sender, MouseEventArgs e)
         {
             this.DragMove();
-            sche.Text = "test";
         }
 
         private void MissionBar_Click(object sender, RoutedEventArgs e)
@@ -58,7 +139,7 @@ namespace ScheduleBell
             int done = Convert.ToInt32((sender as Button).Tag);
             if (done == 0)
             {
-                (sender as Button).Background = new SolidColorBrush(Color.FromRgb(0x55, 0xFF, 0x55));
+                (sender as Button).Background = new SolidColorBrush(Color.FromRgb(0xAA, 0xFF, 0xAA));
                 (sender as Button).Tag = 1;
             }
             else
@@ -79,7 +160,7 @@ namespace ScheduleBell
                 HorizontalContentAlignment = HorizontalAlignment.Center,
                 Height = 36,
                 Width = 220,
-                Background = new SolidColorBrush(Color.FromRgb(0xFF, 0x55, 0x55)),
+                Background = new SolidColorBrush(Color.FromRgb(0xFF, 0xAA, 0xAA)),
                 FontSize = 14,
                 Tag = 0
             };
@@ -87,9 +168,18 @@ namespace ScheduleBell
             list.Children.Add(btn);
         }
 
+        private void MissionName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                AddMissionBar();
+            }
+        }
+
         private void AddMissionBtn_Click(object sender, RoutedEventArgs e)
         {
             AddMissionBar();
         }
+
     }
 }
